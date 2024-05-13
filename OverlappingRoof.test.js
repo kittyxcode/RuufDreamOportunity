2b9d3b238b0a956040270c39c9ecff2688d4b1c2
kittyxcode <katherine.pino059@gmail.com>
2024-05-13 00:14:33 -0400

    fix: fix a little test

--- OverlappingRoof.test.js
+++ OverlappingRoof.test.js
@@ -8,6 +8,6 @@ test('Superposicion Paneles 1x2 y techo 2x4 => Caben 6', () => {
   expect(overlappingRoof(1, 2, 2, 4, 2)).toBe(6);
 });
 
-test('Superposicion Paneles 2x2 y techo 2x4 => Caben', () => {
+test('Superposicion Paneles 2x2 y techo 2x4 => Caben 2', () => {
   expect(overlappingRoof(2, 2, 2, 4, 4)).toBe(2);
 });
