36749c66094c1105dc768d3767146794704e3266
kittyxcode <katherine.pino059@gmail.com>
2024-05-12 23:17:41 -0400

    test: Overlapping test ok

---
+++ OverlappingRoof.test.js
@@ -0,0 +1,13 @@
+const overlappingRoof = require('./OverlappingRoof');
+
+test('Superposicion Paneles 1x2 y techo 2x4 => Caben 7', () => {
+  expect(overlappingRoof(1, 2, 2, 4, 1)).toBe(7);
+});
+
+test('Superposicion Paneles 1x2 y techo 2x4 => Caben 6', () => {
+  expect(overlappingRoof(1, 2, 2, 4, 2)).toBe(6);
+});
+
+test('Superposicion Paneles 2x2 y techo 2x4 => Caben', () => {
+  expect(overlappingRoof(2, 2, 2, 4, 4)).toBe(2);
+});
