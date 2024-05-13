5e0c6301180464563bf02b9e5f88db38a2745b0f
kittyxcode <katherine.pino059@gmail.com>
2024-05-12 23:07:35 -0400

    feat: add TriangularRoof Function

---
+++ TriangularRoof.js
@@ -0,0 +1,7 @@
+function triangularRoof(xPanel, yPanel, baseTriangulo, alturaTriangulo) {
+    const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+    const areaPanel = xPanel * yPanel;
+    return Math.floor(areaTriangulo / areaPanel);
+  }
+  
+  module.exports = triangularRoof;
